19967a59a4ff659255bdb252b62b9f8f4cfff1a4
Carl LeBlond <carl.leblond@iup.edu>
2014-08-30 10:54:19 -0400

    fixed language strings

--- lang/en/qtype_easyomechjs.php
+++ lang/en/qtype_easyomechjs.php
@@ -28,24 +28,24 @@ $string['addmoreanswerblanks'] = 'Blanks for {no} More Answers';
 $string['answermustbegiven'] = 'You must enter an answer if there is a grade or feedback.';
 $string['answerno'] = 'Answer {$a}';
 $string['pluginname'] = 'Electron Pushing / Curved Arrow (MarvinJS)';
-$string['pluginname_help'] = 'Test and strengthen students knowledge of reaction mechanism, resonance and curved arrow notation.  Draw a reaction mechanism or resonance problem in the MarvinSketch applet below.  Be sure to add curved arrows where appropriate...the student will be required to provide them.  Choose from two difficulty levels; Show Products (easier) or Do not Show Products (more difficult).  You can ask questions such as;<br><ul><li>Please add curved arrows showing the flow of electrons for the following reaction?</li><li>Please add curved arrows showing how the following resonance structure could be obtained?</li></ul>';
+$string['pluginname_help'] = 'Test and strengthen students knowledge of reaction mechanism, resonance and curved arrow notation.  Draw a reaction mechanism or resonance problem in MarvinJS below.  Be sure to add curved arrows where appropriate...the student will be required to provide them.  Choose from two difficulty levels; Show Products (easier) or Do not Show Products (more difficult).  You can ask questions such as;<br><ul><li>Please add curved arrows showing the flow of electrons for the following reaction?</li><li>Please add curved arrows showing how the following resonance structure could be obtained?</li></ul>';
 $string['pluginname_link'] = 'question/type/easyomechjs';
 $string['pluginnameadding'] = 'Adding a Electron Pushing / Curved Arrow question (MarvinJS)';
 $string['pluginnameediting'] = 'Editing a Electron Pushing / Curved Arrow question (MarvinJS)';
 $string['pluginnamesummary'] = 'Students must provide curved arrows on a structure or reaction template that you predefine.  You can ask questions such as;<ul><li>Please add curved arrows showing the flow of electrons for the following reaction?</li><li>Please add curved arrows showing how the following resonance structure could be obtained?</li></ul>';
-$string['easyomechjs_options'] = 'Path to Marvin Applet installation';
-$string['enablejava'] = 'Tried but failed to load Marvinsketch editor. You have not got a JAVA runtime environment working in your browser. You will need one to attempt this question.';
-$string['enablejavaandjavascript'] = 'Loading Marvinsketch editor.... If this message does not get replaced by the Marvin editor then you have not got javascript and a JAVA runtime environment working in your browser.';
-$string['configeasyomechjsoptions'] = 'The path of your marvin installation relative to your web root.  (e.g. If your moodle is installed at /var/www/moodle and you install your marvin at /var/www/marvin then you should use the default /marvin)';
+$string['easyomechjs_options'] = 'Path to MarvinJS installation';
+$string['enablejava'] = 'Tried but failed to load MarvinJS editor. You have not got a JAVA runtime environment working in your browser. You will need one to attempt this question.';
+$string['enablejavaandjavascript'] = 'Loading MarvinJS editor.... If this message does not get replaced by the MarvinJS editor then you have not got javascript and a JAVA runtime environment working in your browser.';
+$string['configeasyomechjsoptions'] = 'The path of your marvin installation relative to your web root.  (e.g. If your moodle is installed at /var/www/html/moodle and you install your Marvinjs at /var/www/html/marvinjs then you should use the default /marvinjs)';
 $string['filloutoneanswer'] = '<b><ul>
-<li>Draw a complete reaction or resonance conversion with curved arrows in the MarvinSketch applet below.</li>
+<li>Draw a complete reaction or resonance conversion with curved arrows in the MarvinJS editor below.</li>
 <li>Use multiple arrows for muti-step reactions.</li>
 <li>Choose whether you want the the products removed from the template (Only for single step reactions).</li>
 <li>Choose whether the arrow addition order is important or not.</li>
 <li>Click the "Insert from editor" button when finished.</li>
 <li>Optionally:  Add your complete incorrect reactions/resonance and incorrect arrows to the other answer fields.</li>
 </ul></b>';
-$string['filloutanswers'] = 'Use the Marvinsketch Molecular editor to create the answers, then press the "Insert from editor" buttons to insert the SMILES code into the answer boxes';
+$string['filloutanswers'] = 'Use the MarvinJS Molecular editor to create the answers, then press the "Insert from editor" buttons to insert the SMILES code into the answer boxes';
 $string['insertfromeditor'] = 'Insert from editor';
 $string['javaneeded'] = 'To use this page you need a Java-enabled browser. Download the latest Java plug-in from {$a}.';
 $string['instructions'] = 'The ChemAxon ("mrv") representation of your model must be stored in the following field in order to be graded:';
@@ -55,6 +55,6 @@ $string['correctansweris'] = 'The correct answer is: {$a}.';
 $string['correctanswers'] = '<b>Instructions</b>';
 $string['notenoughanswers'] = 'This type of question requires at least {$a} answers';
 $string['pleaseenterananswer'] = 'Please enter an answer.';
-$string['easyomechjseditor'] = 'MarvinSketch Editor';
+$string['easyomechjseditor'] = 'MarvinJS Editor';
 $string['author'] = 'Question type courtesy of Carl LeBlond, Indiana University of Pennsylvania';
 $string['insert'] = 'Insert from editor';
