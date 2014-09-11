4788dd9010400383524e8b4e712e0437d0c91996
Carl LeBlond <carl.leblond@iup.edu>
2014-09-11 16:09:28 -0400

    updated version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2014083000;
+$plugin->version   = 2014091100;
 $plugin->requires  = 2012062500;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_easyomechjs';
